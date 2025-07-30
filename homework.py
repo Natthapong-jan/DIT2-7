327f55a51d34b8453048459cc319111c1a092601
Natthapong-jan <natthapong-jan@rcc.ac.th>
2025-07-30 13:11:02 +0700

    Add files via upload

--- homework.py
+++ homework.py
@@ -1,2 +1,7 @@
-for i in range(5)
-    
+a = int(input("ใส่ตัวเลขจำนวนเต็ม"))
+b = int(input("ใส่ตัวเลขจำนวนเต็ม"))
+c = int(input("ใส่ตัวเลขจำนวนเต็ม"))
+d = int(input("ใส่ตัวเลขจำนวนเต็ม"))
+e = int(input("ใส่ตัวเลขจำนวนเต็ม"))
+number = [a,b,c,d,e]
+print(sorted(number))
